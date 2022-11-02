34c7218f1047c9bdf6a96377602b66f73bc1f59f
Relax <82351081+Squex01@users.noreply.github.com>
2022-11-02 12:02:43 +0300

    Add files via upload

---
+++ MilitaryTycoon.lua
@@ -0,0 +1,4 @@
+getgenv().Pumpkins = true --true or false for on/off
+getgenv().ZombieNormal = false --true or false for on/off
+getgenv().ZombieSoldier = false --true or false for on/off
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Squex01/MilitayTycoon/main/Militay.lua"))()
